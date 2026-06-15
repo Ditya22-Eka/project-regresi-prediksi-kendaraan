4c81d73580e6185c337dd9f336969ef727c5cc41
Ditya22-Eka <aditekakumara23@gmail.com>
2026-06-16 00:59:38 +0800

    Baca Data Set Dari Excel

--- prediksi_kendaraan.py
+++ prediksi_kendaraan.py
@@ -17,3 +17,26 @@ from sklearn.metrics import (
     accuracy_score,
     classification_report
 )
+
+# 2. MEMBACA DATASET EXCEL
+
+# Mengambil lokasi folder tempat file Python berada
+folder_project = Path(__file__).parent
+
+# Nama file dataset Excel
+file_dataset = folder_project / "DATASET_KENDARAAN_BERMOTOR_SULTENG_2022_2025.xlsx"
+
+# Membaca daftar sheet dalam file Excel
+excel_file = pd.ExcelFile(file_dataset)
+
+print("Daftar sheet dalam file Excel:")
+print(excel_file.sheet_names)
+
+# Membaca sheet Dataset_Model
+data = pd.read_excel(file_dataset, sheet_name="Dataset_Model")
+
+print("\n5 data pertama:")
+print(data.head())
+
+print("\nInformasi dataset:")
+print(data.info())
